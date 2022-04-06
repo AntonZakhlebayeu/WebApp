474d685b144d46b07c2a4549c29f15ed917f6b81
AntonZakhlebayeu <antonzakhlebayeu@gmail.com>
2022-04-06 23:02:04 +0300

    Final

--- WebApp/wwwroot/js/AdminOptions.js
+++ WebApp/wwwroot/js/AdminOptions.js
@@ -1,83 +1,60 @@
 ﻿function deleteCheckedContacts() {
-
-    var objectsToDelete = document.getElementsByClassName("user-checkbox")
-
+    var objectsToDelete = document.getElementsByClassName("user-checkbox");
     var contactIds = [];
-
     for (var i = 0; i < objectsToDelete.length; i++) {
         var objectToDelete = objectsToDelete[i];
         if (objectToDelete.checked)
-            contactIds.push(objectsToDelete[i].getAttribute("name"))
+            contactIds.push(objectsToDelete[i].getAttribute("name"));
     }
-
     if (contactIds.length > 0) {
-
-        var RequestString = "";
-
+        var requestString = "";
         for (var i = 0; i < contactIds.length - 1; i++) {
-            RequestString += "Ids[" + i + "]=" + contactIds[i] + "&"
+            requestString += "Ids[" + i + "]=" + contactIds[i] + "&"
         }
+        requestString += "Ids[" + (contactIds.length - 1) + "]=" + contactIds[contactIds.length - 1] + "";
 
-        RequestString += "Ids[" + (contactIds.length - 1) + "]=" + contactIds[contactIds.length - 1] + ""
-        console.log(RequestString)
-
-        window.location.replace("/home/delete?" + RequestString);
-
+        window.location.replace("/home/delete?" + requestString);
     }
 
 }
 
 function blockCheckedContacts() {
-
-    var objectsToDelete = document.getElementsByClassName("user-checkbox")
-
+    var objectsToBlock = document.getElementsByClassName("user-checkbox")
     var contactIds = [];
-
-    for (var i = 0; i < objectsToDelete.length; i++) {
-        var objectToDelete = objectsToDelete[i];
+    for (var i = 0; i < objectsToBlock.length; i++) {
+        var objectToDelete = objectsToBlock[i];
         if (objectToDelete.checked)
-            contactIds.push(objectsToDelete[i].getAttribute("name"))
+            contactIds.push(objectsToBlock[i].getAttribute("name"));
     }
-
     if (contactIds.length > 0) {
-
-        var RequestString = "";
-
+        var requestString = "";
         for (var i = 0; i < contactIds.length - 1; i++) {
-            RequestString += "Ids[" + i + "]=" + contactIds[i] + "&"
+            requestString += "Ids[" + i + "]=" + contactIds[i] + "&"
         }
-
-        RequestString += "Ids[" + (contactIds.length - 1) + "]=" + contactIds[contactIds.length - 1] + ""
-
-        window.location.replace("/home/block?" + RequestString);
-
+        requestString += "Ids[" + (contactIds.length - 1) + "]=" + contactIds[contactIds.length - 1] + "";
+        
+        window.location.replace("/home/block?" + requestString);
     }
 
 }
 
 function unblockCheckedContacts() {
-
-    var objectsToDelete = document.getElementsByClassName("user-checkbox")
-
+    var objectsToUnblock = document.getElementsByClassName("user-checkbox")
     var contactIds = [];
-
-    for (var i = 0; i < objectsToDelete.length; i++) {
-        var objectToDelete = objectsToDelete[i];
+    for (var i = 0; i < objectsToUnblock.length; i++) {
+        var objectToDelete = objectsToUnblock[i];
         if (objectToDelete.checked)
-            contactIds.push(objectsToDelete[i].getAttribute("name"))
+            contactIds.push(objectsToUnblock[i].getAttribute("name"));
     }
 
     if (contactIds.length > 0) {
-
-        var RequestString = "";
-
+        var requestString = "";
         for (var i = 0; i < contactIds.length - 1; i++) {
-            RequestString += "Ids[" + i + "]=" + contactIds[i] + "&"
+            requestString += "Ids[" + i + "]=" + contactIds[i] + "&"
         }
+        requestString += "Ids[" + (contactIds.length - 1) + "]=" + contactIds[contactIds.length - 1] + "";
 
-        RequestString += "Ids[" + (contactIds.length - 1) + "]=" + contactIds[contactIds.length - 1] + ""
-
-        window.location.replace("/home/unblock?" + RequestString);
+        window.location.replace("/home/unblock?" + requestString);
 
     }
 
